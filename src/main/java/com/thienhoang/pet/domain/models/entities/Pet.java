d12fc89b8ecd1389763b7f8924bdf18a676f5f5a
unknown <huxton.learning@gmail.com>
2025-05-13 12:13:14 +0700

    update

--- src/main/java/com/thienhoang/pet/domain/models/entities/Pet.java
+++ src/main/java/com/thienhoang/pet/domain/models/entities/Pet.java
@@ -3,12 +3,14 @@ package com.thienhoang.pet.domain.models.entities;
 import com.thienhoang.common.models.entities.AuditingEntity;
 import com.thienhoang.pet.domain.models.enums.PetType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Getter
 @Setter
-// @Table(name = "pet")
-// @Entity
+@Table(name = "pet")
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class Pet extends AuditingEntity {
